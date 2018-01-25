ec0841a5126f7dd767d4ad3f7279b41b8f8bb984
DesJC <jcd9930@hotmail.ca>
2018-01-25 12:47:19 -0500

    Code MoveLeft test cases in PaddleTest

--- Pong/PongTests/PaddleTest.cs
+++ Pong/PongTests/PaddleTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PongLibrary;
 
 namespace PongTests
 {
@@ -9,21 +10,49 @@ namespace PongTests
         [TestMethod]
         public void MoveLeft_AlreadyOnBound()
         {
+            Paddle paddleOnBound = GetPaddleOnBound(false);
+            int initialX = paddleOnBound.BoundingBox.X;
+
+            paddleOnBound.MoveLeft();
+            int afterMoveX = paddleOnBound.BoundingBox.X;
+
+            Assert.AreEqual(initialX, afterMoveX);
         }
 
         [TestMethod]
         public void MoveLeft_GoingOutOfBound()
         {
+            int space = 2;
+            Paddle paddle = GetPaddleOneStepFromOutOfBound(space);
+            int limit = paddle.BoundingBox.X - space;
+
+            paddle.MoveLeft();
+
+            Assert.Equals(limit, paddle.BoundingBox.X);
         }
 
         [TestMethod]
         public void MoveLeft_GoingOnBound()
         {
+            int space = 2;
+            Paddle paddle = GetPaddleOneStepFromBound(space);
+            int limit = paddle.BoundingBox.X - space;
+
+            paddle.MoveLeft();
+
+            Assert.Equals(limit, paddle.BoundingBox.X);
         }
 
         [TestMethod]
         public void MoveLeft_GoingInBound()
         {
+            int space = 5;
+            Paddle paddle = GetPaddeOneStepFromInbound(space);
+            int expectedX = paddle.BoundingBox.X - space;
+
+            paddle.MoveLeft();
+
+            Assert.Equals(expectedX, paddle.BoundingBox);
         }
 
         [TestMethod]
@@ -104,6 +133,37 @@ namespace PongTests
         public void Paddle_speedEqualsNot0()
         {
 
+        }
+
+        private Paddle GetPaddleOnBound(Boolean isRightBound)
+        {
+            Paddle paddleOnBound = new Paddle(18, 2, 20, 15, 1);
+
+            if (isRightBound)
+            {
+                paddleOnBound.MoveRight();
+            }
+            else
+            {
+                paddleOnBound.MoveLeft();
+            }
+
+            return paddleOnBound;
+        }
+
+        private Paddle GetPaddleOneStepFromOutOfBound(int pixels)
+        {
+            return new Paddle(20, 2, 20 + (pixels * 2), 15, (pixels + 5));
+        }
+
+        private Paddle GetPaddleOneStepFromBound(int pixels)
+        {
+            return new Paddle(20, 2, 20 + (pixels * 2), 15, pixels);
+        }
+
+        private Paddle GetPaddeOneStepFromInbound(int speed)
+        {
+            return new Paddle(20, 2, 20 + (speed * 4), 15, speed);
         }
     }
 }
